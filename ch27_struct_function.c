7274750846e69ee0d95d9cf36bd6a3d16b391163
gudqo1995 <gudqo1995@naver.com>
2020-07-10 11:49:19 +0900

    동적할당

---
+++ ch27_struct_function.c
@@ -0,0 +1,126 @@
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <time.h>
+//
+////구조체 Academy
+//typedef struct academy {
+//	int no;	  //번호
+//	int cpp;  //점수
+//	int java; //점수
+//	int sum;  //총점
+//}Academy;
+//
+////------ 함수 선언 ---------------------
+//Academy CalcSum(Academy man);
+//void InputPoint(Academy stu[], int stuLength);
+//void PointSum(Academy* man);
+//void PrintPoint(Academy stu[], int stuLength);
+//void ShowTitle();
+//void ShowPoint(Academy man);
+//
+//int BestStu(Academy stu[], int stuLength);
+////선생님꺼
+//int FindTop(Academy* stu, int length);
+//void ShowTopPoint(Academy* stu, int top);
+//
+////------ 메인 함수 ---------------------------
+//int main() {
+//	
+//	/*Academy man = { 1,98,97 };
+//	man = CalcSum(man);
+//	printf("%d - %d, %d, %d\n", man.no, man.cpp, man.java, man.sum);
+//	puts("");*/
+//
+//	Academy stu[3];
+//	int stuLength = sizeof(stu) / sizeof(Academy);
+//
+//	InputPoint(stu, stuLength);
+//	PrintPoint(stu, stuLength);
+//
+//	// 최고 점수 학생의 위치를 찾아서 반환하는 함수를 구현하세요
+//	int topIdx = FindTop(stu, stuLength);
+//
+//	// 최고 점수 학생의 성적을 출력하는 함수를 구현하세요
+//	ShowTopPoint(stu, topIdx);
+//	//내꺼
+//	int BestStudent = BestStu(stu, stuLength);
+//	printf("최고 점수는 %d번 학생 입니다.\n", BestStudent);
+//	
+//}
+//
+////------------------- 함수 정의 ------------------
+//
+//Academy CalcSum(Academy man) {
+//	man.sum = man.cpp + man.java;
+//	return man;
+//}
+//
+//void InputPoint(Academy stu[], int stuLength) { //배열은 주소니까 값 바로 바껴서 void하면 됨
+//	srand(time(NULL));
+//	puts("----- 점수 입력 -----");
+//	for (int i = 0; i < stuLength; i++) {
+//		//직접입력하기
+//		//stu[i].no = i + 1;
+//		//printf("cpp 점수 입력 : ");
+//		//scanf_s("%d", &stu[i].cpp);
+//		//printf("java 점수 입력 : ");
+//		//scanf_s("%d", &stu[i].java);
+//		//stu[i].sum = stu[i].cpp + stu[i].java;
+//
+//		stu[i].no = i + 1;
+//		stu[i].cpp = rand() % 91 + 10;
+//		stu[i].java = rand() % 91 + 10;
+//		PointSum(stu + i); //PointSum(&stu[i]);
+//	}
+//}
+//
+//void PointSum(Academy* man) {
+//	man->sum = man->cpp + man->java;
+//}
+//
+//void PrintPoint(Academy stu[], int stuLength) {
+//	puts("----- 학생 성적 -----");
+//	ShowTitle();
+//	for (int i = 0; i < stuLength; i++) {
+//		ShowPoint(stu[i]);
+//	}
+//}
+//
+//void ShowTitle() {
+//	printf("%4s %8s %8s %8s\n", "번호", "cpp", "java", "총점");
+//}
+//
+//void ShowPoint(Academy man) {
+//	printf("%4d %8d %8d %8d\n", man.no, man.cpp, man.java, man.sum);
+//}
+//
+////최고 점수 학생 위치 찾기
+//int BestStu(Academy stu[], int stuLength) {
+//	int n = stu[0].sum;
+//	for (int i = 1; i < stuLength; i++) {
+//		if (n < stu[i].sum)
+//			n = stu[i].sum;
+//	}
+//	printf("최고 점수 :%d\n", n);
+//	for (int i = 0; i < stuLength; i++) {
+//		if (stu[i].sum == n) {
+//			printf("최고 점수 학생 성적: cpp-%d, java-%d, 총점-%d\n", stu[i].cpp, stu[i].java, stu[i].sum);
+//			return stu[i].no;
+//		}
+//	}
+//}
+////선생님꺼
+//void ShowTopPoint(Academy* stu, int top) {
+//	puts("-----  최 고 점 수 학 생  -----");
+//	ShowPoint(stu[top]);
+//}
+//
+//int FindTop(Academy* stu, int length) {
+//	int top = 0;
+//	for (int i = 1; i < length; i++) {
+//		if (stu[i].sum > stu[top].sum)
+//			top = i;
+//	}
+//	return top;
+//}
+//
---
+++ ch28_dynamic-allocation.cpp
@@ -0,0 +1,136 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+//void LifeCycle() {
+//	//지역변수 - stack
+//	int a = 10;
+//	//동적할당 - heap
+//	int* p = (int*)malloc(sizeof(int)); // 함수 안에 동적할당 사용하면 함수 끝나고 나서도 사용할수 있음
+//	*p = 20;
+//
+//}
+//
+//int main() {
+//	//동적할당 사용방법
+//	//int* p = (int*)malloc(sizeof(int));
+//	//*p = 10;
+//	//printf("*p : %d\n", *p);
+//	//free(p);
+//
+//	//지역변수랑 동적할당 차이점
+//	LifeCycle();
+//	//LifeCycle()함수가 끝나도 동적할당으로 힙영역에 남아있는 데이터는 지워지지 않음
+//	//함수가 끝나도 함수 안에 썻던 데이터를 사용하고 싶으면 동적할당을 사용, 위치만 알고 있으면됨
+//
+//}
+
+//-------------------------------------------------------------------------------------------
+
+//int* IntegerAlloc() { //주소값을 반환할수도 있음
+//	return (int*)malloc(sizeof(int));
+//}
+//
+//double* DoubleAlloc() {
+//	return (double*)malloc(sizeof(double));
+//}
+//
+//char* CharAlloc() {
+//	return (char*)malloc(sizeof(char));
+//}
+//
+//typedef struct { //기본이름 생략가능(자기 참조 구조체로 포인터 변수 사용하면 생략 불가)
+//	int a;
+//	double b;
+//}Data;
+//
+//Data* DataAlloc() {
+//	return (Data*)malloc(sizeof(Data));
+//}
+//
+//int main() {
+//	int* ip = IntegerAlloc();
+//	*ip = 10;
+//	printf("*ip : %d\n", *ip);
+//	puts("");
+//
+//	double* dp = DoubleAlloc();
+//	*dp = 2.2;
+//	printf("*dp : %.1f\n", *dp);
+//	puts("");
+//
+//	char* cp = CharAlloc();
+//	*cp = 'a';
+//	printf("*cp : %c\n", *cp);
+//	puts("");
+//
+//	Data* pdata = DataAlloc();
+//	pdata->a = 100;
+//	pdata->b = 5.5;
+//	printf("%d - %.1f\n", pdata->a, pdata->b);
+//	puts("");
+//
+//
+//	free(ip);
+//	free(dp);
+//	free(cp);
+//	free(pdata);
+//}
+
+
+//-------------------------------------------------------------------------------------------
+//문자열
+
+//char* InputString() {
+//	char tmp[50];
+//
+//	printf("단어 입력 : ");
+//	scanf_s("%s", tmp, sizeof(tmp));
+//
+//	char* s = (char*)malloc(strlen(tmp) + 1);
+//	if (s != NULL) {
+//		strcpy_s(s, strlen(tmp) + 1, tmp);
+//	}
+//	return s;
+//}
+//
+//int main() {
+//	char* stn = NULL; //문자는 널값으로 초기화
+//	
+//	stn = InputString();
+//	printf("%s\n", stn);
+//
+//	free(stn);
+//}
+
+
+//-------------------------------------------------------------------------------------------
+
+//int main() {
+//	//int arr[300000];
+//	//stack에서는 프로그램 사용할 용량만 있기 때문에 적은 용량만 가지고 있음
+//	//큰 데이터를 쓸려면 힙영역을 사용해야됨
+//
+//	int size = 300000;
+//
+//	int* arr = (int*)malloc(sizeof(int) * size); //배열 크기 원하는 만큼 조절 가능
+//	//int arr[300000]; //먹통임, stack 영역 초과
+//
+//	srand((unsigned int)time(NULL));
+//	if (arr != NULL) {
+//		for (int i = 0; i < size; i++) {
+//			//*(arr + i) = rand() % 99 + 1;
+//			arr[i] = rand() % 99 + 1;
+//			printf("%3d ", arr[i]);
+//			if ((i + 1) % 10 == 0)
+//				puts("");
+//		}
+//	}
+//
+//	free(arr);
+//
+//	//int arr[300000]; 이렇게 하면 stack 메모리를 써서 이 정도 크기는 돌아가지 않음
+//	//동적할당으로 heap영역쓰면 대용량 데이터 사용 가능
+//	//꼭 사용하고 지워줘야됨
+//}
